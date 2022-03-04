5ae4e3a6594b29c392cc2f1f692c9d1e79c05088
shyam205 <mastershyam2013@gmail.com>
2022-03-04 21:08:28 +0530

    commented some auth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "../../../lib/mongodb";
 import GithubProvider from "next-auth/providers/github";
-import EmailProvider from "next-auth/providers/email";
+//import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
-import SpotifyProvider from "next-auth/providers/spotify";
-import NetlifyProvider from "next-auth/providers/netlify";
-import LinkedInProvider from "next-auth/providers/linkedin";
-import InstagramProvider from "next-auth/providers/instagram";
+// import FacebookProvider from "next-auth/providers/facebook";
+// import SpotifyProvider from "next-auth/providers/spotify";
+// import NetlifyProvider from "next-auth/providers/netlify";
+// import LinkedInProvider from "next-auth/providers/linkedin";
+// import InstagramProvider from "next-auth/providers/instagram";
 
 
 export default NextAuth({
@@ -25,40 +25,40 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
    
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-    }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    // }),
 
-    SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET
-    }),
+    // SpotifyProvider({
+    //   clientId: process.env.SPOTIFY_CLIENT_ID,
+    //   clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    // }),
 
-    NetlifyProvider({
-      clientId: process.env.NETLIFY_CLIENT_ID,
-      clientSecret: process.env.NETLIFY_CLIENT_SECRET
-    }),
+    // NetlifyProvider({
+    //   clientId: process.env.NETLIFY_CLIENT_ID,
+    //   clientSecret: process.env.NETLIFY_CLIENT_SECRET
+    // }),
      
-    LinkedInProvider({
-      clientId: process.env.LINKEDIN_CLIENT_ID,
-      clientSecret: process.env.LINKEDIN_CLIENT_SECRET
-    }),
-    InstagramProvider({
-      clientId: process.env.INSTAGRAM_CLIENT_ID,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
-      from: process.env.EMAIL_FROM
-    }),
+    // LinkedInProvider({
+    //   clientId: process.env.LINKEDIN_CLIENT_ID,
+    //   clientSecret: process.env.LINKEDIN_CLIENT_SECRET
+    // }),
+    // InstagramProvider({
+    //   clientId: process.env.INSTAGRAM_CLIENT_ID,
+    //   clientSecret: process.env.INSTAGRAM_CLIENT_SECRET
+    // }),
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: process.env.EMAIL_SERVER_PASSWORD
+    //     }
+    //   },
+    //   from: process.env.EMAIL_FROM
+    // }),
 
 
   // Providers.Spotify({
